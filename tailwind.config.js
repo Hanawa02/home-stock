39e25afc8e3ce4695af0123f542dea28773ce47e
Laura Caroline <laura@laura-melo.com>
2024-03-24 20:18:48 +0100

    feat: add card-shadow style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const GRAY_BASE_HS = '0, 5%'
+
 export default {
   content: ['./index.html', './src/**/*.vue'],
   theme: {
-    extend: {}
+    extend: {
+      boxShadow: {
+        'card': `0px 1px 4px hsla(${GRAY_BASE_HS}, 0%, 0.25)`,
+        'card-hover': `0px 1px 4px 2px hsla(${GRAY_BASE_HS}, 40%, 0.3)`
+      }
+    }
   },
   plugins: []
 }
