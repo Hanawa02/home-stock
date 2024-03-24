b3dab4fe84ad9a694878dad1bccccfdcbd0fedd3
Laura Caroline <laura@laura-melo.com>
2024-03-24 20:13:34 +0100

    Add pinia and vuefire to storybook preview

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,22 @@
-import type { Preview } from '@storybook/vue3'
+import { type Preview, setup } from '@storybook/vue3'
+import { type App } from 'vue'
 
 // This is needed to apply tailwind and other styles to the stories preview
 import '../src/assets/main.css'
+
+import { createPinia } from 'pinia'
+
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from '../src/firebase'
+
+setup((app: App) => {
+  app.use(createPinia())
+
+  app.use(VueFire, {
+    firebaseApp,
+    modules: [VueFireAuth()]
+  })
+})
 
 const preview: Preview = {
   parameters: {
