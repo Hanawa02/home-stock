836bb1d238f5ac8c5ea8d4f6f3f8bf1d249310a8
Laura Caroline <laura@laura-melo.com>
2024-03-24 21:25:56 +0100

    fix: add missing translation

--- src/views/RootView.vue
+++ src/views/RootView.vue
@@ -2,16 +2,23 @@
 import { computed } from 'vue'
 import { useHomeStore } from '@/stores/homes'
 import routes from '@/router/routes'
+import { useLocalization } from '@/composables/localization'
+import translations from './translations'
 
 const homeStore = useHomeStore()
 const homes = computed(() => homeStore.homes.value)
 
 const homeRouteName = routes.homes.item.name
+
+const localization = useLocalization()
+const translation = computed(() => translations[localization.locale.value])
 </script>
 
 <template>
   <main>
-    <h1 class="text-3xl text-center font-bold py-4 lg:text-5xl text-blue-600">Homes</h1>
+    <h1 class="text-3xl text-center font-bold py-4 lg:text-5xl text-blue-600">
+      {{ translation.rootView.homes() }}
+    </h1>
     <ul class="p-4 grid md:grid-cols-2 lg:grid-cols-4">
       <li v-for="home in homes" :key="home.id">
         <router-link :to="{ name: homeRouteName, params: { id: home.id } }">
