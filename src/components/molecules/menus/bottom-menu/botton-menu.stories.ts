f65cf5dc281296971c0d47f13d488261749d0dda
Laura Caroline <laura@laura-melo.com>
2024-04-08 22:04:26 +0200

    Add bottom menu component

---
+++ src/components/molecules/menus/bottom-menu/botton-menu.stories.ts
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import BottomMenu from './index.vue'
+
+const meta = {
+  title: 'Components/Menus/BottomMenu',
+  component: BottomMenu,
+
+  argTypes: {
+    menuItems: {
+      control: {
+        type: 'object'
+      }
+    }
+  }
+} satisfies Meta<typeof BottomMenu>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Menu: Story = {
+  args: {
+    menuItems: [
+      {
+        label: 'Home',
+        icon: 'home',
+        clickFunction: () => {
+          console.log('Home clicked')
+        }
+      },
+      {
+        label: 'Shopping List',
+        icon: 'list',
+        clickFunction: () => {
+          console.log('Shopping List clicked')
+        }
+      }
+    ]
+  }
+}
---
+++ src/components/molecules/menus/bottom-menu/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-full p-4 border-t shadow bg-white">
+    <nav>
+      <ul class="flex justify-evenly">
+        <li v-for="menu in menuItems" :key="menu.label" class="w-full">
+          <button
+            @click="menu.clickFunction"
+            class="flex flex-col items-center group mx-auto"
+            :aria-label="menu.label"
+          >
+            <span class="text-amber-800 md:group-hover:text-amber-700 material-symbols-outlined">
+              {{ menu.icon }}
+            </span>
+            <label
+              class="text-zinc-600 md:group-hover:text-amber-800 md:group-hover:underline font-normal cursor-pointer"
+              >{{ menu.label }}</label
+            >
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script setup lang="ts">
+export interface MenuItem {
+  label: string
+  icon: string
+  clickFunction: () => void
+}
+
+interface IProps {
+  menuItems: MenuItem[]
+}
+
+defineProps<IProps>()
+</script>
