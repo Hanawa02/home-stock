0bbb171a7619018277d2a6d7a77d1cdcb1e96b97
Laura Caroline <laura@laura-melo.com>
2024-04-14 21:03:00 +0200

    feat: add vuetify components

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,13 @@ import router from './router'
 import { VueFire, VueFireAuth } from 'vuefire'
 import { firebaseApp } from './firebase'
 
+import { vuetify } from './vuetify'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.use(VueFire, {
   firebaseApp,
