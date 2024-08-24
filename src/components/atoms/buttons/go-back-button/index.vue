b67d050d9780700f1e801528aba4e64b627171b9
Laura Caroline <laura@laura-melo.com>
2024-08-24 18:40:12 +0200

    chore: remove vuetify and add vue mdi

--- src/components/atoms/buttons/go-back-button/index.vue
+++ src/components/atoms/buttons/go-back-button/index.vue
@@ -1,15 +1,18 @@
 <template>
   <button
     @click="goBack"
-    class="flex shrink-0 p-2 rounded-full text-blue-600 hover:bg-blue-50 hover:text-blue-800 hover:shadow-card"
+    :class="[
+      'flex shrink-0 p-2 rounded-full text-base',
+      'text-blue-600 hover:bg-blue-50 hover:text-blue-800 hover:shadow-card'
+    ]"
   >
-    <span class="material-symbols-outlined w-6 h-6"> arrow_back </span>
+    <ArrowLeftIcon class="w-6 h-6" />
   </button>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
-
+import ArrowLeftIcon from 'icons/ArrowLeft.vue'
 const router = useRouter()
 
 function goBack(): void {
--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,11 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'icons': fileURLToPath(new URL('./node_modules/vue-material-design-icons', import.meta.url))
     }
   }
 })
