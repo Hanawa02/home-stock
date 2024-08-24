699882812f31777fb0affba75a92a43cfd93e2df
Laura Caroline <laura@laura-melo.com>
2024-08-24 20:39:25 +0200

    fix: add aria label to go back button

--- src/components/atoms/buttons/go-back-button/index.vue
+++ src/components/atoms/buttons/go-back-button/index.vue
@@ -5,7 +5,7 @@
       'flex shrink-0 p-2 rounded-full text-base',
       'text-indigo-600 hover:bg-indigo-50 hover:text-indigo-800 hover:shadow-card'
     ]"
-    aria-label=""
+    :aria-label="go_back_button__arial_label()"
   >
     <ArrowLeftIcon class="w-6 h-6" />
   </button>
@@ -14,6 +14,9 @@
 <script setup lang="ts">
 import { useRouter } from "vue-router"
 import ArrowLeftIcon from "~icons/ArrowLeft.vue"
+
+import { go_back_button__arial_label } from "~/i18n"
+
 const router = useRouter()
 
 function goBack(): void {
